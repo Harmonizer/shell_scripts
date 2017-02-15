ab57f76ebd41ddd203d79cbe2f5a1b2a128750d8
Harmonizer <pm.moors@gmail.com>
2017-02-15 23:57:32 +0100

    record_check.sh

---
+++ record_check.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -p
+
+# Checks for given domain name if target IP address matches the given IP address.
+# When the given IP addresses no longer match, user will be notified. Script could be
+# useful if you wants to determine if a recently modified A record is in effect. 
+
+# Write secure shell scripts
+set -euf -o pipefail
+
+# Current record target IP address
+IPADDR="0.0.0.0"
+
+# Domain (or subdomain)
+DOMAIN="example.example.com"
+
+# Query domain and extract target IP
+LOOKUP_RESULT=`nslookup ${DOMAIN} \
+  | grep Address \
+  | grep -v \# \
+  | cut -d' ' -f2`
+
+# Show given input
+echo "IPADDR =" ${IPADDR}
+echo "DOMAIN =" ${DOMAIN}
+
+# Print time stamp + target IP address for given domain
+while [ "${LOOKUP_RESULT}" = "${IPADDR}" ]
+do
+    echo "[`date +%k:%M:%S`]" ${DOMAIN} "=" ${LOOKUP_RESULT}
+    sleep 2
+done
+
+# Notify user if record target IP has changed (alert)
+notify-send -u critical "[!]`date +%k:%M:%S` - record_check.sh: \"modified record detected\"" "\nTarget IP address for ${DOMAIN} changed.\n\nNew IP address: ${LOOKUP_RESULT} (old address is: ${IPADDR})"
+
+exit 0
